64802bc7d950528265c594d727d988185b0bdd1f
daishinmutaku <daishinmutaku@gmail.com>
2019-09-25 02:13:34 +0900

    modelの追加

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/daishinmutaku/quest_board_server
 
 go 1.12
+
+require github.com/gin-contrib/cors v1.3.0 // indirect
--- server/main.go
+++ server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func main() {
@@ -17,4 +18,59 @@ func Hello(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "Hello World!",
 	})
+}
+
+// Model
+type User struct {
+    Id int64
+    Name string `sql:"size:255"`
+    CreatedAt time.Time
+    UpdatedAT time.Time
+    DeletedAt time.Time
+}
+
+type Tag struct {
+    Id int64
+    Name string `sql:"size:255"`
+    CreatedAt time.Time
+    UpdatedAT time.Time
+    DeletedAt time.Time
+}
+
+type Quest struct {
+    Id int64
+	Name string 
+	Capacity int64
+	MemberDescription string 
+	QuestDescription string 
+	Period time.Time
+	Reward string 
+	IsFinished bool
+	Producer User
+	Member []User
+	Tag Tag
+    CreatedAt time.Time
+    UpdatedAT time.Time
+    DeletedAt time.Time
+}
+
+type Application struct {
+	Id int64
+	Status int64
+	Quest Quest
+	Applicant User
+    CreatedAt time.Time
+    UpdatedAT time.Time
+    DeletedAt time.Time
+}
+
+type Message struct {
+    Id int64
+	Body string
+	SendTime time.Time
+	Quest Quest
+	Sender User
+    CreatedAt time.Time
+    UpdatedAT time.Time
+    DeletedAt time.Time
 }
